befc3a01b475feeac2615f6944eebd0df53ecb3a
tejaswi <tejaswi0623@gmail.com>
2026-02-20 15:57:55 -0500

    message

--- catalogue.sh
+++ catalogue.sh
@@ -7,6 +7,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+Script_dir=$PWD
+mongodb_ip="mongodb.jarugula.online"
 
 if [ $userid -ne 0 ]; then
   echo -e "$R please run this script with root user access $N" |tee -a $logs_file
@@ -31,7 +33,7 @@ validate $? "enabling nodejs 20 version"
 dnf install nodejs -y &>>$logs_file
 validate $? "install nodejs"
 
-id system_user &>>$logs_file
+id system_user &>>$logs_file #id username if output is not zero create user else skip it
 if [ $3 -ne 0 ]; then
   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$logs_file
   validate $? "creating system user"
@@ -48,7 +50,37 @@ validate $? "downloading catalogue code"
 cd /app 
 validate $? "moving to app directory"
 
-unzip /tmp/catalogue.zip
+rm -rf /app/* #if you run the code again it will ask to download it again so we are removing the code which already has and downloading now
+validate $? "removing the existing code"
+
+unzip /tmp/catalogue.zip &>>$logs_file
 validate $? "unzipping the catalogue code"
+
+npm install
+validate $? "installing dependencies"
+
+cp $Script_dir/catalogue.service /etc/systemd/system/catalogue.service #created catalogue.service script and copying 
+validate $? "copying catalogue service file"
+
+systemctl daemon reload
+systemctl enable catalogue
+systemctl start catalogue
+validate $? "enable the start the catalogue"
+
+cp $Script_dir/mongodb.repo /etc/yum.repos.d/mongo.repo #copying mongo repo 
+dnf install mongodb-mongosh -y
+
+
+data=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")') #vairable=$(command)
+
+if [ $data -ne 0 ]; then
+  mongosh --host  $mongodb_ip </app/db/master-data.js
+  validate $? "loading products"
+else
+  echo -e "products or data is already loaded....$Y skipping $N"
+fi
+
+systemctl restart catalogue
+validate $? "restarting the catalogue"
 
 
