0a8841e0d36f8bdb219543bcb1814338b2da9c22
tejaswi <tejaswi0623@gmail.com>
2026-02-21 16:59:48 -0500

    msg

--- user.sh
+++ user.sh
@@ -2,7 +2,7 @@
 
 userid=$(id -u)
 logs_folder="var/log/shell-script"
-logs_file="$logs_folder/$0/log"
+logs_file="$logs_folder/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -34,15 +34,15 @@ validate $? "enable nodejs 20 version"
 dnf install nodejs -y &>>$logs_file
 validate $? "installing nodejs"
 
-id roboshop &>>$LOGS_FILE
+id roboshop &>>$logs_file
 if [ $? -ne 0 ]; then
-  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$logs_file
   validate $? "creating system user"
 else
   echo -e "roboshop user already exists.....$Y skipping $N"
 fi
 
-mkdir /app &>>$LOGS_FILE
+mkdir /app &>>$logs_file
 validate $? "create app directory"
 
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
@@ -51,13 +51,13 @@ validate $? "downloading user code"
 cd /app 
 validate $? "moving to app directory"
 
-unzip /tmp/user.zip &>>$LOGS_FILE
+unzip /tmp/user.zip &>>$logs_file
 validate $? "unzipping the user code"
 
-npm install &>>$LOGS_FILE
+npm install &>>$logs_file
 validate $? "installing dependencies"
 
-cp $ript_dir/user.service  /etc/systemd/sytem/user.service
+cp $script_dir/user.service  /etc/systemd/sytem/user.service
 validate $? "systemctl services"
 
 systemctl daemon-reload
