aecb8b0c0038e25e69901a407d49b64f64341b6a
tejaswi <tejaswi0623@gmail.com>
2026-02-21 19:46:28 -0500

    msg

--- frontend.sh
+++ frontend.sh
@@ -1,52 +1,59 @@
 #!/bin/bash
 
-USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
+userid=$(id -u)
+logs_folder="/var/log/shell-script"
+logs_file="$logs_folder/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m"
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws88s.online
 
-if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
-    exit 1
-fi
+mkdir -p $logs_folder
 
-mkdir -p $LOGS_FOLDER
+if [ $userid -ne 0 ]; then
+  echo -e " $R please run the script with root user access $N" |tee -a $logs_file
+  exit 1
+fi 
 
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
-    fi
+validate(){
+  if [ $1 -ne 0 ]; then
+   echo -e "$2 is........$R failed $N" |tee -a $logs_file
+   exit 1
+  else
+    echo -e "$2 is......$G success $N" |tee -a $logs_file
+  fi   
 }
 
-dnf module disable nginx -y &>>$LOGS_FILE
-dnf module enable nginx:1.24 -y &>>$LOGS_FILE
-dnf install nginx -y &>>$LOGS_FILE
-VALIDATE $? "Installing Nginx"
+dnf module disable nginx -y &>>$logs_file
+validate $? "disable default version"
 
-systemctl enable nginx  &>>$LOGS_FILE
-systemctl start nginx 
-VALIDATE $? "Enabled and started nginx"
+dnf module enable nginx:1.24 -y &>>$logs_file
+validate $? "enable nginx 1.24 version"
+
+dnf install nginx -y &>>$logs_file
+validate $? "installing nginx"
+
+systemctl enable nginx &>>$logs_file
+systemctl start nginx
+validate $? "enable and start nginx"
 
 rm -rf /usr/share/nginx/html/* 
-VALIDATE $? "Remove default content"
+validate $? "remove nginx default content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+validate $? "download the frontend code"
+
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>>$LOGS_FILE
-VALIDATE $? "Downloaded and unzipped frontend"
+validate $? "moving to nginx html folder"
 
-rm -rf /etc/nginx/nginx.conf
+unzip /tmp/frontend.zip &>>$logs_file
+validate $? "unzipping the frontend code"
+
+rm -rf /etc/nginx/nginx.conf #remove default content of service if script runs again
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "Copied our nginx conf file"
+validate $? "creating systemctl service"
 
-systemctl restart nginx
-VALIDATE $? "Restarted Nginx"
+systemctl restart nginx &>>$logs_file
+validate $? "restarting nginx"
+
