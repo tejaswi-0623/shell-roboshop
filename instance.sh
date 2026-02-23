c39976c2a3d244b6328a9708c7ab145dd95d4c3f
tejaswi <tejaswi0623@gmail.com>
2026-02-23 09:57:58 -0500

    msg

--- instance.sh
+++ instance.sh
@@ -28,7 +28,7 @@ do
             --instance-ids $instance_id \
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text)
-     record_name="$instance.$domain_name" # instancename.daws88s.online
+     record_name="$instance.$domain_name" # instancename.jarugula.online
   fi
     echo "Ip address is....$IP_address"
 #we are getting ip addresses of instance to create route 53 records 
